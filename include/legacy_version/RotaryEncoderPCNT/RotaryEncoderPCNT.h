389441e1cfef4fe7e7e616851c41cc3d6848b249
David <david.alenhag@outlook.com>
2025-06-15 22:43:07 +0200

    Create RotaryEncoderPCNT.h

---
+++ include/legacy_version/RotaryEncoderPCNT/RotaryEncoderPCNT.h
@@ -0,0 +1,34 @@
+#ifndef ROTARYENCODERPCNT_H
+#define ROTARYENCODERPCNT_H
+
+#include <Arduino.h>
+#include "driver/pcnt.h"
+
+class RotaryEncoderPCNT
+{
+public:
+    RotaryEncoderPCNT(int pinA, int pinB,
+                      int16_t lowLimit = -INT16_MAX,
+                      int16_t highLimit = INT16_MAX);
+    ~RotaryEncoderPCNT();
+
+    bool      begin();
+    void      deinit();
+    int32_t   getPosition();
+    void      setPosition(int32_t pos);
+    void      zero();
+
+private:
+    gpio_num_t   _pinA;
+    gpio_num_t   _pinB;
+    int16_t      _lowLimit;
+    int16_t      _highLimit;
+    pcnt_unit_t  _unit;
+    bool         _started;
+    int32_t      _offset;
+
+    // assign units 0…PCNT_UNIT_3
+    static pcnt_unit_t  _nextUnit;
+};
+
+#endif  // ROTARYENCODERPCNT_H
